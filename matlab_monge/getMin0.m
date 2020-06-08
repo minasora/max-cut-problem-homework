5aa72825f64e49c3ff1ecf3683033f2a71e5036d
qcbegin <48995827+qcbegin@users.noreply.github.com>
2020-06-08 21:56:05 +0800

    Add getMin function
    添加了task3中的两个getMin0函数，整理了目录

---
+++ matlab_monge/getMin0.m
@@ -0,0 +1,30 @@
+function [a,b] = getMin0(A)
+% [a,b] = getMin0(A)
+% 返回矩阵A中最小元素的行列索引
+% 实现MATLAB内置函数min，根据官方说明，若有多个最小元素，只返回第一个元素的行列索引
+
+% 获取矩阵A的行数与列数
+[m,n] = size(A); 
+
+% 初始化变量
+min_val = A(1,1);
+min_row = 1;
+min_col = 1;
+
+% 遍历寻找最小值
+for i = 1:m
+    for j = 1:n
+       temp = A(i,j);
+       if temp < min_val
+          min_val = temp;
+          min_row = i;
+          min_col = j;
+       end
+    end
+end
+
+% 返回最小元素位置
+a = min_row;
+b = min_col;
+end
+

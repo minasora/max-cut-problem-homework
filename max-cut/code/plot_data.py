badf16bec507c079eabfd76e31d1b25b9d9f6cb0
qcbegin <48995827+qcbegin@users.noreply.github.com>
2020-07-15 21:27:26 +0800

    部分修正

--- max-cut/code/plot_data.py
+++ max-cut/code/plot_data.py
@@ -53,3 +53,61 @@ def plot_iter(iters,objs,best_objs, first_line, file):
     # 存储并显示图线
     plt.savefig('./image/'+file.replace('.txt','.png'))
     plt.show()
+
+def tabu_plots(files):
+    """
+    读取多个禁忌搜索迭代过程数据并绘制对应目标值变化图线
+    :param files:记录文件所在目录列表
+    :return:
+    """
+    iters = [[],[],[],[],[]]
+    objs = [[],[],[],[],[]]
+    best_objs = [[],[],[],[],[]]
+    tabu_lens =[]
+
+    for i in range(5):
+        with open(files[i],'r') as f:
+            data = f.read()
+            iter_datas = data.split('\n\n')
+            iter_datas.pop()  # 剔除末尾空值
+            first_line = iter_datas.pop(0)  # 从数据中移除并读取第一行
+            tabu_lens.append(first_line.split()[1])  # 读取禁忌长度
+
+            for iter_data in iter_datas:
+                lines = iter_data.split('\n')
+                # 获取对应数据
+                iter = lines[0].split()[1]
+                iters[i].append(int(iter))
+                obj = lines[2].split()[1]
+                objs[i].append(int(obj))
+                best_obj = lines[3].split()[1]
+                best_objs[i].append(int(best_obj))
+
+    plt.figure(figsize=(16, 9), dpi=150)
+    for i in range(len(tabu_lens)):
+        # 绘制图线
+        plt.plot(iters[i], objs[i],linewidth=2)
+
+    legends = ['temp_obj(tabu_len ={})'.format(tabu_len) for tabu_len in tabu_lens]
+    plt.legend(legends)
+    plt.xlabel('iters')
+    plt.ylabel('objective')
+    plt.title('Objective Value during the Iteration')
+
+    plt.savefig('./image/tabu_all.png')
+    plt.show()
+
+    plt.figure(figsize=(16, 9), dpi=150)
+    for i in range(len(tabu_lens)):
+        # 绘制图线
+        plt.plot(iters[i], best_objs[i],linewidth=2)
+
+    legends = ['best_obj(tabu_len ={})'.format(tabu_len) for tabu_len in tabu_lens]
+    plt.legend(legends)
+    plt.xlabel('iters')
+    plt.ylabel('objective')
+    plt.title('Objective Value during the Iteration')
+
+    # 存储并显示图线
+    plt.savefig('./image/tabu_all_best.png')
+    plt.show()
--- max-cut/code/test1.py
+++ max-cut/code/test1.py
@@ -1,65 +1,7 @@
-import matplotlib.pyplot as plt
 import tabu_search as ts
 import max_cut_instance as m_instance
 import copy
-
-def plot(files):
-    """
-    读取迭代过程数据并绘制对应目标值变化图线
-    :param files:记录文件所在目录列表
-    :return:
-    """
-    iters = [[],[],[],[],[]]
-    objs = [[],[],[],[],[]]
-    best_objs = [[],[],[],[],[]]
-    tabu_lens =[]
-
-    for i in range(5):
-        with open(files[i],'r') as f:
-            data = f.read()
-            iter_datas = data.split('\n\n')
-            iter_datas.pop()  # 剔除末尾空值
-            first_line = iter_datas.pop(0)  # 从数据中移除并读取第一行
-            tabu_lens.append(first_line.split()[1])  # 读取禁忌长度
-
-            for iter_data in iter_datas:
-                lines = iter_data.split('\n')
-                # 获取对应数据
-                iter = lines[0].split()[1]
-                iters[i].append(int(iter))
-                obj = lines[2].split()[1]
-                objs[i].append(int(obj))
-                best_obj = lines[3].split()[1]
-                best_objs[i].append(int(best_obj))
-
-    plt.figure(figsize=(16, 9), dpi=150)
-    for i in range(len(tabu_lens)):
-        # 绘制图线
-        plt.plot(iters[i], objs[i],linewidth=2)
-
-    legends = ['temp_obj(tabu_len ={})'.format(tabu_len) for tabu_len in tabu_lens]
-    plt.legend(legends)
-    plt.xlabel('iters')
-    plt.ylabel('objective')
-    plt.title('Objective Value during the Iteration')
-
-    plt.savefig('./image/tabu_all.png')
-    plt.show()
-
-    plt.figure(figsize=(16, 9), dpi=150)
-    for i in range(len(tabu_lens)):
-        # 绘制图线
-        plt.plot(iters[i], best_objs[i],linewidth=2)
-
-    legends = ['best_obj(tabu_len ={})'.format(tabu_len) for tabu_len in tabu_lens]
-    plt.legend(legends)
-    plt.xlabel('iters')
-    plt.ylabel('objective')
-    plt.title('Objective Value during the Iteration')
-
-    # 存储并显示图线
-    plt.savefig('./image/tabu_all_best.png')
-    plt.show()
+from plot_data import tabu_plots
 
 # 测试不同禁忌长度下的禁忌搜索算法
 instance = m_instance.Problem_Instance("text.txt")
@@ -73,4 +15,4 @@ tabu_solution = ts.tabu_search(solution2, instance, 0.08, 1000)
 tabu_solution = ts.tabu_search(solution3, instance, 0.16, 1000)
 tabu_solution = ts.tabu_search(solution4, instance, 0.24, 1000)
 tabu_solution = ts.tabu_search(solution5, instance, 0.32, 1000)
-plot(['tabu{}.txt'.format(i) for i in [5,10,20,30,40]])
+tabu_plots(['tabu{}.txt'.format(i) for i in [5,10,20,30,40]])
